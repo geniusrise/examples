ac08edd8a4f35b4d973028480fb754c9ce7fc24c
ixaxaar <root@ixaxaar.in>
2024-01-18 15:14:10 +0530

    fix map_data and lora config

--- cli/fine_tune/text/txtclass/bert-base-uncased__yelp_review_full.sh
+++ cli/fine_tune/text/txtclass/bert-base-uncased__yelp_review_full.sh
@@ -41,7 +41,7 @@ genius TextClassificationFineTuner rise \
             map_data=None \
             hf_repo_id="ixaxaar/test" \
             hf_commit_message="yay" \
-            hf_token="***REMOVED***" \
+            hf_token="" \
             hf_private=true \
             hf_create_pr=False \
             notification_email="russi@geniusrise.ai" \
--- cli/fine_tune/text/txtclass/txtclass.sh
+++ cli/fine_tune/text/txtclass/txtclass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-genius NLIFineTuner rise \
+genius TextClassificationFineTuner rise \
     batch \
         --input_s3_bucket geniusrise-test \
         --input_s3_folder input/txtclass \
@@ -20,7 +20,7 @@ genius NLIFineTuner rise \
             model_name="google/flan-t5-base" \
             tokenizer_name="google/flan-t5-base" \
             num_train_epochs=3 \
-            per_device_batch_size=4 \
+            per_device_batch_size=8 \
             model_class="AutoModelForSequenceClassification" \
             tokenizer_class="AutoTokenizer" \
             device_map="cuda:0" \
@@ -31,19 +31,20 @@ genius NLIFineTuner rise \
             use_trl=False \
             accelerate_no_split_module_classes="[]" \
             evaluate=False \
-            save_steps=5000 \
+            save_steps=15000 \
             save_total_limit=None \
             load_best_model_at_end=False \
             metric_for_best_model=None \
             greater_is_better=None \
             use_huggingface_dataset=True \
-            huggingface_dataset="multi_nli" \
+            huggingface_dataset="AmazonScience/massive" \
             map_data=None \
-            hf_repo_id="ixaxaar/flan-t5-base_multi-nli" \
+            hf_repo_id="ixaxaar/flan-t5-base_massive" \
             hf_commit_message="yay" \
             hf_token="" \
             hf_private=true \
             hf_create_pr=False \
             notification_email="russi@geniusrise.ai" \
             compile=False \
-            learning_rate=5e-5
+            learning_rate=5e-5 \
+            map_data="lambda d: {'label': d['intent'], 'text': d['utt']}"
