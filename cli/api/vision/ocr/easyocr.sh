4492282fcb4018a48a0084af2e8cc94efecb20de
ixaxaar <root@ixaxaar.in>
2024-02-16 02:24:57 +0530

    add nougat and donut

--- cli/api/vision/ocr/easyocr.sh
+++ cli/api/vision/ocr/easyocr.sh
@@ -24,8 +24,20 @@ genius ImageOCRAPI rise \
             username="user" \
             password="password"
 
-(base64 -w 0 cat.jpg | awk '{print "{\"image_base64\": \""$0"\"}"}' > /tmp/image_payload.json)
-curl -X POST http://localhost:3000/api/v1/classify_image \
+(base64 -w 0 test_images_ocr/ReceiptSwiss.jpg | awk '{print "{\"image_base64\": \""$0"\"}"}' > /tmp/image_payload.json)
+curl -X POST http://localhost:3000/api/v1/ocr \
+    -H "Content-Type: application/json" \
+    -u user:password \
+    -d @/tmp/image_payload.json | jq
+
+(base64 -w 0 test_images_ocr/django.jpg | awk '{print "{\"image_base64\": \""$0"\"}"}' > /tmp/image_payload.json)
+curl -X POST http://localhost:3000/api/v1/ocr \
+    -H "Content-Type: application/json" \
+    -u user:password \
+    -d @/tmp/image_payload.json | jq
+
+(base64 -w 0 test_images_ocr/pdf.jpg | awk '{print "{\"image_base64\": \""$0"\"}"}' > /tmp/image_payload.json)
+curl -X POST http://localhost:3000/api/v1/ocr \
     -H "Content-Type: application/json" \
     -u user:password \
     -d @/tmp/image_payload.json | jq
--- cli/api/vision/ocr/naver-clova-ix__donut-base-finetuned-cord-v2.sh
+++ cli/api/vision/ocr/naver-clova-ix__donut-base-finetuned-cord-v2.sh
@@ -16,15 +16,25 @@ genius ImageOCRAPI rise \
         --postgres_table state \
     listen \
         --args \
-            model_name="paddleocr" \
+            model_name="naver-clova-ix/donut-base-finetuned-cord-v2" \
+            model_class="VisionEncoderDecoderModel" \
+            processor_class="DonutProcessor" \
             device_map="cuda:0" \
+            use_cuda=True \
+            precision="float" \
+            quantization=0 \
+            max_memory=None \
+            torchscript=False \
+            compile=False \
+            flash_attention=False \
+            better_transformers=False \
             endpoint="*" \
             port=3000 \
             cors_domain="http://localhost:3000" \
             username="user" \
             password="password"
 
-(base64 -w 0 test_images_ocr/ReceiptSwiss.jpg | awk '{print "{\"image_base64\": \""$0"\"}"}' > /tmp/image_payload.json)
+(base64 -w 0 test_images_ocr/ReceiptSwiss.jpg | awk '{print "{\"image_base64\": \""$0"\", \"max_length\": 1024}"}' > /tmp/image_payload.json)
 curl -X POST http://localhost:3000/api/v1/ocr \
     -H "Content-Type: application/json" \
     -u user:password \
