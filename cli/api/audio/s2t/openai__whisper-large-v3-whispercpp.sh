4acae6de3e184be9d9bb70e714bca027e7431c5d
ixaxaar <root@ixaxaar.in>
2024-02-27 16:12:49 +0530

    add whisper cpp

---
+++ cli/api/audio/s2t/openai__whisper-large-v3-whispercpp.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+genius SpeechToTextAPI rise \
+    batch \
+            --input_folder ./input \
+    batch \
+            --output_folder ./output \
+    none \
+    listen \
+        --args \
+            model_name="large" \
+            use_whisper_cpp=True \
+            endpoint="*" \
+            port=3000 \
+            cors_domain="http://localhost:3000" \
+            username="user" \
+            password="password"
+
+(base64 -w 0 sample.mp3 | awk '{print "{\"audio_file\": \""$0"\"}"}' > /tmp/payload.json)
+curl -X POST http://localhost:3000/api/v1/transcribe \
+    -H "Content-Type: application/json" \
+    -u user:password \
+    -d @/tmp/payload.json | jq
+
+
+(base64 -w 0 sample.flac | awk '{print "{\"audio_file\": \""$0"\"}"}' > /tmp/payload.json)
+curl -X POST http://localhost:3000/api/v1/transcribe \
+    -H "Content-Type: application/json" \
+    -u user:password \
+    -d @/tmp/payload.json | jq
